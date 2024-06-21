b9cab702a38fafe6be4c8849cf48fc7cbf62319d
Raulcasallas26 <rcasallas26@gmail.com>
2024-06-21 17:48:19 -0500

    nueva url 21/06

--- Frontend/src/routes/direccion.js
+++ Frontend/src/routes/direccion.js
@@ -1,9 +1,9 @@
 import {ref} from "vue"
 
-const urlBackend= "http://localhost:3500"
+const urlBackend= "https://repsena.onrender.com"
 
-// local = ""
-// render = "https://repsena.onrender.com"
+// local = "http://localhost:3500"
+// render = ""
 
 let load = ref( false )
 
